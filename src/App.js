b5bcc400d1c150716f0fdbfbd259453be8fae9f3
rakeshpawar7848 <rakesh.pawar@exathought>
2024-01-10 17:52:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginMessage, setLoginMessage] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/login', { username, password }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      // Check if login was successful
+      if (response.data.success) {
+        setLoginMessage(response.data.message);
+      } else {
+        setLoginMessage(response.data.message); // Display the error message
+        console.error('Login unsuccessful:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login Page</h1>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Login</button>
+      <p>{loginMessage}</p>
     </div>
   );
-}
+};
 
 export default App;
